6fcf65f1ee12d482ef81369a39bd76fa9762ce56
DaithiOCheallaigh <dave.kelly@tripadmit.com>
2026-02-24 14:37:44 +0000

    Fix Vite base path for GitHub Pages subdirectory

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,10 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  viteFinal: (config) => {
+    config.base = '/MarshDesign/'
+    return config
+  },
 }
 
 export default config
